e6a86e6fffc105d0c1a5c820daf218ecfa97ec29
nivaldoo <47394762+nivaldoo@users.noreply.github.com>
2019-04-16 09:24:57 -0300

    Add files via upload

---
+++ API/Controllers/UsuarioController.cs
@@ -0,0 +1,52 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        // GET: api/Usuario
+        //public IEnumerable<UsuarioViewModel> Get()
+        public IHttpActionResult Get()
+        {
+            return Ok(UsuarioModel.Usuarios());
+        }
+
+        // GET: api/Usuario/5
+        public IHttpActionResult Get(string id, string nome, string cpf)
+        {
+            //return Ok(UsuarioModel.Usuario(id)); //Um só
+            var filter = new UsuarioViewModel { Id = id, Nome = nome, CPF = cpf };
+            return Ok(UsuarioModel.Usuarios(filter));
+            //return "value";
+        }
+
+        // POST: api/Usuario
+        public void Post([FromBody]dynamic value)
+        {
+        }
+
+        // PUT: api/Usuario/5
+        public void Put(string id, [FromBody]dynamic value)
+        {
+        }
+
+        // DELETE: api/Usuario/5
+        public void Delete(string id)
+        {
+            //throw new Exception("Errou");
+        }
+
+        [HttpPost]
+        [Route("api/Usuario/Outro")]
+        public IHttpActionResult Outro([FromBody]dynamic value)
+        {
+            return Ok(value);
+        }
+    }
+}
---
+++ API/Models/UsuarioModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class UsuarioModel
+    {
+        public static UsuarioViewModel Usuario(string id)
+        {
+            using (var context = new autenticadorEntities())
+            {
+                Guid _id = new Guid(id);
+                return new UsuarioViewModel(context.usuario.Find(_id));
+            }
+        }
+        public static IEnumerable<UsuarioViewModel> Usuarios(UsuarioViewModel filter = null)
+        {
+            //throw new Exception("Errou");
+
+            using (var context = new autenticadorEntities())
+            {
+                var usuarios = new List<UsuarioViewModel>();
+
+                IQueryable<usuario> query = context.Set<usuario>();
+
+                if (filter != null)
+                {
+                    if (!string.IsNullOrEmpty(filter.Id))
+                    {
+                        Guid Id = new Guid(filter.Id);
+                        query = query.Where(n => n.usu_id_usuario == Id);
+                    }
+                    if (!string.IsNullOrEmpty(filter.Nome))
+                    {
+                        query = query.Where(n => n.usu_ds_nome.Contains(filter.Nome));
+                    }
+                    if (!string.IsNullOrEmpty(filter.CPF))
+                    {
+                        string CPF = filter.CPF.Replace("-", "").Replace(".", "").Replace(" ", "");
+                        query = query.Where(n => n.usu_ds_cpf.Replace("-", "").Replace(".", "").Replace(" ", "") == CPF);
+                    }
+                }
+
+                query.ToList().ForEach(obj =>
+                {
+                    usuarios.Add(new UsuarioViewModel(obj));
+                });
+
+                return usuarios;
+            }
+        }
+    }
+
+    public class UsuarioViewModel
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public UsuarioViewModel() { }
+        public UsuarioViewModel(usuario usuario)
+        {
+            this.Nome = usuario.usu_ds_nome;
+        }
+    }
+}
